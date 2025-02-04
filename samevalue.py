6f8bc9d0f5cdc016ff1f877d28df4bed9cbc9a63
Santiago Tena Hernandez <stenahe@gmail.com>
2025-02-04 14:13:54 +0100

    change loaded dataset

--- samevalue.py
+++ samevalue.py
@@ -257,11 +257,13 @@ def build_parameters():
   random_seed = 42
 
   datasets = [
-    					{
-                'name': 'dry_bean',
-								'id': 602,
-              },
-            ]
+      # {'name': 'dry_bean',
+      #   'id': 602,},
+      {'name': 'isolet',
+       'id': 54, },
+      # {'name': 'musk_v2',
+      #   'id': 75,},
+  ]
 
   gnn_model = {
                 'hidden_dim': [16],
